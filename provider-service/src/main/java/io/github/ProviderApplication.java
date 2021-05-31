412ac85979bbd8090ac88dc31c56da1aafe57e80
e <18009838166@163.com>
2021-06-01 05:42:40 +0800

    add mysql datasource for provider-service.

--- provider-service/src/main/java/io/github/ProviderApplication.java
+++ provider-service/src/main/java/io/github/ProviderApplication.java
@@ -1,11 +1,13 @@
 package io.github;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan("io.github.dao")
 public class ProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
--- provider-service/src/main/java/io/github/entity/UserEntity.java
+++ provider-service/src/main/java/io/github/entity/UserEntity.java
@@ -1,9 +1,18 @@
 package io.github.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import io.github.constant.TableConstant;
 import lombok.Data;
 
 @Data
+@TableName(TableConstant.USER)
 public class UserEntity {
+
+    /**
+     * id
+     */
+    private Long id;
 
     /**
      * 用户名
